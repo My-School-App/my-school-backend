84cbdc187d80c648d3e88c536166dd3fa6e53d13
KOSSY <abonekosiso@gmail.com>
2022-06-14 12:47:55 +0100

    broilerplate code

---
+++ database.js
@@ -0,0 +1,6 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+const { MONGODB_URI } = process.env
+
+
+
---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => 
+    res.send('route working')
+)
+
+app.listen(3000, () => console.log('server started successfully'))
