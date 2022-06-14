175005c0ff5ee09da8eea496cf05caf453bb8a1e
KOSSY <abonekosiso@gmail.com>
2022-06-14 13:01:28 +0100

    database setup

--- database.js
+++ database.js
@@ -3,4 +3,9 @@ const mongoose = require('mongoose')
 const { MONGODB_URI } = process.env
 
 
-
+mongoose.connect(MONGODB_URI)
+    .then(() => console.log('DB is Connected...'))
+    .catch(err => {
+        console.log('DB Connection Failed!!! ' + err);
+        process.exit();
+    })
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
-
 const app = express()
+const db = require('./database')
 
 app.get('/', (req, res) => 
     res.send('route working')
