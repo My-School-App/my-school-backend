e6e1b309763606d583d60eb4d087b39fc64db510
KOSSY <abonekosiso@gmail.com>
2022-06-15 23:38:19 +0100

    creating controllers

---
+++ controllers/student.controller.js
@@ -0,0 +1,5 @@
+const Student = require('../models/student.model')
+
+exports.createStudent = async(req, res) => {
+    
+}
--- models/student.model.js
+++ models/student.model.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const Student = new Schema({
+const studentSchema = new Schema({
     firstName: {type: String, required},
     LastName: {type: String, required},
     class: {type: String, required},
     subjects: {type: [String], required}
 })
+
+const Student = mongoose.model('Student', studentSchema)
+module.exports(Student);
