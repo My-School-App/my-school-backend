b98825952e4f3758552ec5bcd140b81f932b1836
owolana ayomide <ayomideowolana1@gmail.com>
2022-06-16 16:52:27 +0100

    added view, styles and logic for assignment upload

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const { PORT } = process.env
-const db = require('./database')
-const studentRoute = require('./routes/student.route')
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const { PORT } = process.env;
+const db = require("./database");
+const studentRoute = require("./routes/student.route");
+const path = require("path");
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => 
-    res.send('Home route working.')
-)
+// allows express to use absolute file paths
+const options = {
+  root: path.join(__dirname)
+};
 
-app.use('/student', studentRoute)
+// allows express serve static files
+app.use(express.static("public"));
 
-app.listen(PORT, () => console.log(`Server started successfully at port ${PORT}`))
+app.get("/", (req, res) => res.send("Home route working."));
+
+app.use("/student", studentRoute);
+
+// upload assignments route
+app.get("/upload-assignment", (req, res) =>
+  res.sendFile("/public/pages/assignment-upload.html", options)
+);
+
+app.listen(PORT, () =>
+  console.log(`Server started successfully at port ${PORT}`)
+);
