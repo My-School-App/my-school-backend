d537a1c82b15a2482d8d907f640d4d7cd767a9a3
KOSSY <abonekosiso@gmail.com>
2022-06-18 14:05:59 +0100

    added new props to studentSchema

--- models/student.model.js
+++ models/student.model.js
@@ -5,6 +5,8 @@ const studentSchema = new Schema({
     id: {type: String, required: true, unique: true},
     firstName: {type: String, required: true},
     lastName: {type: String, required: true},
+    age: {type: Number, required: true},
+    yearJoined: {type: Date, required: true},
     currentClass: {type: String, required: true},
     subjects: {type: [String], required: true}
 })
